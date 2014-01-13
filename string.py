c168800fe96eb15dc5ada62deb8fdddae3a6aa2a
lisawei <187055815@qq.com>
2014-01-13 18:12:16 +0800

    update method of string

--- string.py
+++ string.py
@@ -3,3 +3,7 @@ quest=raw_input("what's you quest")
 color=raw_input("what is you favorite color")
 
 print "Ah, so your name is %s, your quest is %s, your favorite color is %s." % (name,quest,color)
+
+my_string="wow, you're great"
+print len(my_string)
+print my_string.upper()
