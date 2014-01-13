f0dfe3b701cbaf26f8836f90e58a9ee4f283cd56
lisawei <187055815@qq.com>
2014-01-13 18:06:05 +0800

    python string

---
+++ string.py
@@ -0,0 +1,5 @@
+name=raw_input("what's your name")
+quest=raw_input("what's you quest")
+color=raw_input("what is you favorite color")
+
+print "Ah, so your name is %s, your quest is %s, your favorite color is %s." % (name,quest,color)
